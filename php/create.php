2207f07c5bc8bcbcf02afb7e90b471bd31ff5f6e
mintumintu <41105213+mintumintu@users.noreply.github.com>
2018-07-22 22:06:25 +0530

    Add files via upload

---
+++ php/create.php
@@ -0,0 +1,38 @@
+<html>
+<head>
+  <link rel="stylesheet" type="text/css" href="style.css">
+  <title>Create</title>
+</head>
+<body>
+  <div class="bgimage">
+    <div class="form">
+      <table>
+        <form method="post" action="acountcreate.php">
+          <tr>
+            <th id="header">Create Your account</th>
+          </tr>
+          <tr>
+            <td> Full Name<br>
+              <input type="text">
+            </td>
+          </tr>
+          <tr>
+              <td>Email<br>
+              <input type="email">
+            </td>
+          </tr>
+          <tr>
+            <td>New Password<br>
+              <input type="password">
+            </td>
+          </tr>
+          <tr>
+            <td><input type="submit">
+            </td>
+          </tr>
+        </form>
+      </table>
+    </div>
+  </div>
+</body>
+</html>
